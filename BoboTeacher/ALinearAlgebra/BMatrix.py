90f5d8df6e4420f50e672d0f35461ab25601b052
yulin.tian <yulin.tian@dmall.com>
2019-03-30 17:05:53 +0800

    矩阵变换

--- BoboTeacher/ALinearAlgebra/BMatrix.py
+++ BoboTeacher/ALinearAlgebra/BMatrix.py
@@ -7,6 +7,11 @@ class Matrix:
     def __init__(self, list2d):
         self._values = [row[:] for row in list2d]
 
+    @classmethod
+    def zero(cls, r, c):
+        """返回一个r行c列的0矩阵"""
+        return cls([[0] * c for _ in range(r)])
+
     # 复写打印方法
     def __repr__(self):
         return "Matrix({})".format(self._values)
@@ -44,3 +49,49 @@ class Matrix:
     def col_vector(self, index):
         """返回矩阵第index个列向量"""
         return Vector([row[index] for row in self._values])
+
+    def __add__(self, other):
+        """矩阵的加法运算"""
+        assert self.shape() == other.shape(), "Error in adding. Shape of matrix must be same."
+        return Matrix([[a + b for a, b in zip(self.row_vector(i), other.row_vector(i))] for i in range(self.row_num())])
+
+    def __sub__(self, other):
+        """矩阵的减法运算"""
+        assert self.shape() == other.shape(), "Error in adding. Shape of matrix must be same."
+        return Matrix([[a - b for a, b in zip(self.row_vector(i), other.row_vector(i))] for i in range(self.row_num())])
+
+    def __mul__(self, k):
+        """矩阵数乘，返回self * k的结果"""
+        return Matrix([[e * k for e in self.row_vector(i)] for i in range(self.row_num())])
+
+    def __rmul__(self, k):
+        """矩阵数乘，返回k * self的结果"""
+        return self * k
+
+    def __truediv__(self, k):
+        """矩阵数除，返回self / k的结果"""
+        return self * (1 / k)
+
+    def __pos__(self):
+        """返回矩阵取正的结果"""
+        return 1 * self
+
+    def __neg__(self):
+        """返回矩阵取负的结果"""
+        return -1 * self
+
+    def dot(self, other):
+        """矩阵点乘"""
+        if isinstance(other, Vector):
+            # 矩阵和向量的点乘
+            assert self.col_num() == len(other), "Error in Matrix-Vector Multiplication."
+            return Vector([self.row_vector(i).dot(other) for i in range(self.row_num())])
+
+        if isinstance(other, Matrix):
+            # 矩阵和矩阵的点乘
+            assert self.col_num() == other.row_num(), "Error in Matrix-Matrix Multiplication."
+            return Matrix([[self.row_vector(i).dot(other.col_vector(j)) for j in range(other.col_num())] for i in range(self.row_num())])
+
+    def T(self):
+        """返回矩阵的转置矩阵"""
+        return Matrix([[e for e in self.col_vector(i)] for i in range(self.col_num())])
--- BoboTeacher/ALinearAlgebra/main_matrix.py
+++ BoboTeacher/ALinearAlgebra/main_matrix.py
@@ -1,8 +1,12 @@
 from BoboTeacher.ALinearAlgebra.BMatrix import Matrix
+from BoboTeacher.ALinearAlgebra.AVector import Vector
+import numpy as np
+import matplotlib.pyplot as plt
+import math
 
 
-if __name__ == "__main__":
-
+# 自己的matrix类的测试
+def my_matrix_test():
     matrix = Matrix([[1, 2], [3, 4], [5, 6]])
     print(matrix)
     print(matrix.shape())
@@ -10,7 +14,80 @@ if __name__ == "__main__":
     print(matrix.col_num())
     print(matrix.size())
     print(len(matrix))
-    print(matrix[2, 1])
+    print(matrix[:, 1])
     print(matrix.row_vector(1))
     print(matrix.col_vector(1))
+    print("-----------------------")
+    matrix2 = Matrix([[1, 2], [3, 4], [5, 6]])
+    print(matrix + matrix2)
+    print("-----------------------")
+    print(matrix - matrix2)
+    print("-----------------------")
+    print(matrix * 3)
+    print(3 * matrix)
+    print("-----------------------")
+    print(Matrix.zero(2, 3))
+    print("-----------------------")
+    vector = Vector([1, 2])
+    print(matrix2.dot(vector))
+    print("-----------------------")
+    matrix3 = Matrix([[1, 2], [3, 4]])
+    print(matrix.dot(matrix3))
+    print("-----------------------")
+    print(matrix.T())
+    print(matrix3.T())
+
+
+# numpy中的matrix的测试
+def np_matrix_test():
+    a = np.array([[1, 2], [3, 4]])
+    print(a)
+    print(a.shape)  # 形状
+    print(a.T)  # 转置
+    print(a[1, 1])  # 获取矩阵元素
+    print(a[0])  # 获取矩阵一个行向量
+    print(a[:, 1])  # 获取矩阵一个列向量
+    print("-----------------------")
+    b = np.array([[5, 6], [7, 8]])
+    print(a + b)  # 矩阵加法
+    print(a - b)  # 矩阵减法
+    print(10 * a)  # 矩阵数乘
+    print(a * b)  # 矩阵对应元素乘
+    print(a.dot(b))  # 矩阵乘法
+    print("-----------------------")
+    c = np.array([10, 100])
+    print(a + c)  # 矩阵加向量
+    print(a + 1)  # 广播机制
+    print(a.dot(c))  # 矩阵乘向量
+
+
+# 矩阵变换
+def matrix_transformation_test():
+    points = [[0, 0], [0, 5], [3, 5], [3, 4], [1, 4],
+              [1, 3], [2, 3], [2, 2], [1, 2], [1, 0]]
+    x = [point[0] for point in points]
+    y = [point[1] for point in points]
+    plt.xlim(-10, 10)
+    plt.ylim(-10, 10)
+    plt.plot(x, y)
+
+    P0 = Matrix(points)
+
+    # T = Matrix([[2, 0], [0, 1.5]])  # 缩放变换
+    # T = Matrix([[1, 0], [0, -1]])  # 翻转变换
+    # T = Matrix([[1, 0.5], [0, 1]])  # 错切变换
+    # T = Matrix([[1, 0.5], [0, 1]])  # 错切变换
+    theta = math.pi / 3  # 1/3弧度，等于60度
+    T = Matrix([[math.cos(theta), math.sin(theta)], [-math.sin(theta), math.cos(theta)]])  # 旋转变换
+
+    P1 = T.dot(P0.T())
+    plt.plot([P1.col_vector(i)[0] for i in range(P1.col_num())], [P1.col_vector(i)[1] for i in range(P1.col_num())])
+
+    plt.show()
+
+
+
+
+if __name__ == "__main__":
+    matrix_transformation_test()
 
